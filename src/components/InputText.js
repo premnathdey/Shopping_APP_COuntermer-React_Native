8f3e794e1bae0ddf49a0bb192b0b3a23e78ddc3d
Prem Nath <36821368+premnathdey@users.noreply.github.com>
2020-02-02 01:01:50 +0530

    Update InputText.js

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -2,7 +2,40 @@ import PropTypes from "prop-types";
 import React, {Component} from "react";
 import {TextInput, Text, View, StyleSheet} from "react-native";
 
+const propTypes = {
+    mapElement: PropTypes.func,
+    onSubmitEditing: PropTypes.func,
+    onChangeText: PropTypes.func,
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    maxLength: PropTypes.number,
+    keyboardType: PropTypes.string,
+    secureTextEntry: PropTypes.bool,
+    label: PropTypes.string
+};
 
+const defaultProps = {
+    mapElement: (n) => {},
+    onSubmitEditing: () => {},
+    onChangeText: () => {},
+    value: "",
+    placeholder: "",
+    maxLength: 200,
+    keyboardType: "default",
+    secureTextEntry: false,
+    label: ""
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    width:300,
+    backgroundColor:'rgba(255, 255,255,0.2)',
+    borderRadius: 25,
+    paddingHorizontal:16,
+    fontSize:16,
+    color:'#ffffff',
+    marginVertical: 10
+  }
 
 });
 
